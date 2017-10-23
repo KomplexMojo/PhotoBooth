d6d47e3f2a948104a4a5d539f164c8a62575c962
Blackwater Passive Home Build <darren.ditto@alumni.unbc.ca>
2017-10-22 20:55:18 -0700

    Update

--- Interface.py
+++ Interface.py
@@ -108,7 +108,7 @@ app.setEntryMaxLength("email", 50)
 # link the buttons to the function called press
 app.setSticky("nw")
 app.addIconButton("Email", confirm, "mail", 1, 2)
-app.addNamedButton("CLOSE", "emailwin", app.hideSubWindow)
+app.addNamedButton("CLOSE", "emailwin", app.distroySubWindow)
 app.stopSubWindow()
 
 # start the GUI
